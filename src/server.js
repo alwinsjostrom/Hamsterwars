9ee989e2666bdd8d99b3dfbbbc00532ddb2b5e3c
alwinsjostrom <alwinsjostrom@yahoo.com>
2021-09-23 12:52:37 +0200

    Port

--- src/server.js
+++ src/server.js
@@ -23,5 +23,5 @@ app.use('/hamsters', hamsterRouter)
 
 //Kör igång servern
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:' + PORT);
+    console.log('Server running on port: ' + PORT);
 })
